bd64a8487c7a02786bf5908f63607ba4c7d120d5
Harry Marr <harry.marr@gmail.com>
2018-10-20 22:49:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Go modules internals
+
+The internals of Go modules, extracted from github.com/golang/go. All
+occurrences of `internal` in import paths have been rewritten to `_internal_`,
+but the code is otherwise unchanged.
+
+## Re-extracting
+
+The `extract/` directory contains the code to do this. Run the Go program, then
+copy the contents of the `out/github.com/dependabot/gomodules-extracted`
+directory to the top level of this repo.
---
+++ extract/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"path/filepath"
+	"strings"
+
+	"github.com/hmarr/pkgextract"
+)
+
+func main() {
+	extractor := pkgextract.NewPackageExtractor(pkgextract.PackageExtractorOptions{
+		InitialPkg: "cmd/go/internal/modload",
+		OutputRoot: "./out",
+		ImportRewriter: func(importPath string) string {
+			newPath := strings.Replace(importPath, "internal", "_internal_", -1)
+			return filepath.Join("github.com/dependabot/gomodules-extracted", newPath)
+		},
+		Filter: func(importPath string) bool {
+			return strings.Contains(importPath, "internal")
+		},
+	})
+
+	if err := extractor.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
